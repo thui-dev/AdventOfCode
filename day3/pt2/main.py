fd2f37ab0c7314b0dfa6978a2ab5754531eb1677
tuthui-dev <tuthui.dev@gmail.com>
2024-12-27 21:07:13 -0300

    a

--- day3/pt2/main.py
+++ day3/pt2/main.py
@@ -1,16 +1,36 @@
 
 import re
 
+switch = True
+
 def mul(a, b):
-	return a*b
+	if switch == True:
+		return a*b
+	else:
+		return None
+
+def do():
+	global switch
+	switch = True
+	return
+
+def dont():
+	global switch
+	switch = False
+	return
+
+def clear_donts(a):
+	if a == "don't()":
+		return "dont()"
+	return a
 
 instructions = re.findall("mul\([0-9]*,[0-9]*\)|do\(\)|don't\(\)", open("../input.txt").read())
 
-print(*l, sep='\n')
+instructions = list(map(clear_donts, instructions)) #cant define function "don't()", so changing all occurences of it to "dont()"
 
-switch = True
+#print(*instructions, sep='\n')
+#print(*list(map(eval, instructions)), sep="\n")
 
-for item in instructions:
-	pass
+instructions = list(map(eval, instructions))
 
-#print(sum(list(map(eval, l))))
+print(f"total sum: {sum([_ for _ in instructions if _ is not None])}")
