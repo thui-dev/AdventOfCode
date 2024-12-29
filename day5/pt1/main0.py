833ed89ebdf64a1c765d318a02caf37cd2ea4f04
tuthui-dev <tuthui.dev@gmail.com>
2024-12-29 16:43:11 -0300

    finished optimizing and performance testing

--- day5/pt1/main0.py
+++ day5/pt1/main0.py
@@ -1,9 +1,4 @@
-
-#interesting fact: i noticed lines 15 and 17 are REALLY not optimized, especially given python SETS are
-#very slow, so i tried to test it, and there you go: those lines were being called each 41.000 times.
-#in main02, i made it 40x faster by doing this 1.000 times, and saving in a dict.
-
-def main01():
+def main0():
 	f = open("../input.txt").read().split('\n\n')
 	rules = f[0].split('\n')
 	rules = [list(int(i) for i in line.split('|')) for line in rules]
@@ -20,4 +15,4 @@ def main01():
 
 	print(sum([line[len(line)//2] for line in filter(check, updates)]))
 
-main01()
+main0()
--- day5/pt1/performance_test.py
+++ day5/pt1/performance_test.py
@@ -1,42 +1,27 @@
 
-from main01 import main01
-from main02 import main02
-
 import timeit
 import cProfile
+import sys
+import os
 
-after_rules = {}
-	before_rules = {}
-	after_seen = set()
-	before_seen = set()
-	for i in rules:
-		if i[0] not in after_seen:
-			after_seen.add(i[0])
-			after_rules[i[0]] = set(j[1] for j in rules if j[0] == i[0])
-		if i[1] not in before_rules:
-			before_seen.add(i[1])
-			before_rules[i[1]] = set(j[0] for j in rules if j[1] == i[1])
-
-bf_af_rules = [{}, {}]
-bf_af_seen =  [set(), set()]
-for n in range(2):
-	for rule in rules:
-		if rule[n] not in bf_af_seen[n-1]:
-			bf_af_seen[n].add(rule[n])
-			bf_af_rules[n][rule[n]] = set(_[n-1] for _ in rules if _[n] == rule[n])
-
+SCRIPTS = 2
 RUNS = 100
-print(F"TESTING SCRIPT 1 for {RUNS} times...")
-script1 = timeit.timeit("main01()", globals=globals(), number=RUNS)
-print(f"TESTING SCRIPT 2... for {RUNS} times:")
-script2 = timeit.timeit("main02()", globals=globals(), number=RUNS)
 
-print(f"Average execution time of SCRIPT 1 with {RUNS} runs: {script1 / 100}")
-print(f"Average execution time of SCRIPT 2 with {RUNS} runs: {script2 / 100}")
+script = [None]*SCRIPTS
+for i in range(SCRIPTS):
+	print(F"TESTING SCRIPT {i} for {RUNS} times...")
+	sys.stdout = open(os.devnull, 'w') # Suppress prints
+	script[i] = timeit.timeit(f"main{i}()", setup=f"from main{i} import main{i}", number=RUNS)
+	sys.stdout = sys.__stdout__  # Restore stdout
+
+for i in range(3):
+	print(f"Average execution time of SCRIPT {i} with {RUNS} runs: {script[i] / 100}")
 
 """
-cProfile.run("main01()")
-print()
-print()
-cProfile.run("main02()")
+for i in range(SCRIPTS):
+	sys.stdout = open(os.devnull, 'w') # Suppress prints
+	exec(f"from main{i} import main{i}")
+	sys.stdout = sys.__stdout__  # Restore stdout
+
+	cProfile.run(f"main{i}()")
 """
