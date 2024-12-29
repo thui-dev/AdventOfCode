40e5861fed806200d8b4428e6cb112838ec2799e
tuthui-dev <tuthui.dev@gmail.com>
2024-12-28 21:33:18 -0300

    finsished day 4 pt 2

--- day4/pt2/main.py
+++ day4/pt2/main.py
@@ -4,41 +4,49 @@ def main():
 
 	file = open("../input.txt").read().split('\n')
 
+	def check_for_word(word, index, direction):
+		for char in word:
+			if index[0] < 0 or index[1] < 0:
+				return 0
+			try:
+				if char != file[index[0]][index[1]]:
+					return 0
+			except:
+				return 0
+			index[0] += direction[0]
+			index[1] += direction[1]
+		return 1
+
 	def search(start_index):
 
 		word = "MAS"
-		middle_letter = word[math.ceil(len(word)/2)]
+		middle_letter = word[len(word)//2]
 
-		#8 directions (kernel?)
-		kernel = [
-			[-1,-1], [-1,+1],
-			[+1,-1], [+1,+1]
+		if middle_letter != file[start_index[0]][start_index[1]]:
+			return 0
+
+		diagonal_kernels = [
+			[
+				[-1,-1],
+						[+1,+1]
+			],
+			[
+						[-1,+1],
+				[+1,-1]
+			]
 		]
 
-		final = 0
+		#backtracking from middle
+		tests = 0
+		for diagonals in range(2):
+			for direction in range(2):
+				current_index = list(start_index)
+				for i in range(len(word)//2):
+					current_index[0] += diagonal_kernels[diagonals][direction][0]
+					current_index[1] += diagonal_kernels[diagonals][direction][1]
+				tests += check_for_word(word, current_index, diagonal_kernels[diagonals][direction-1])
 
-		for item in kernel:
-			if middle_letter != file[start_index[0]][start_index[1]]:
-				break
-			
-			current_index = list(start_index)
-			
-			for char in word:
-				if current_index[0] < 0 or current_index[1] < 0:
-					break
-				try:
-					if char != file[current_index[0]][current_index[1]]:
-						break
-				except:
-					break
-				current_index[0] += item[0]
-				current_index[1] += item[1]
-
-			else:
-				final += 1
-				#print("position:", start_index)
-
-		return final
+		return 1 if tests == 2 else 0
 	
 	total = 0
 
