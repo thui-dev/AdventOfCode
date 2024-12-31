b418d81463cd0541c4b8e398e756393389b9f69b
tuthui-dev <tuthui.dev@gmail.com>
2024-12-31 00:19:42 -0300

    PERFECT IMPLEMENTATION OF THE WRONG LOGIC AAAAAAAAAAAAAAAAAAAAAA

--- day6/pt2/main.py
+++ day6/pt2/main.py
@@ -1,16 +1,45 @@
-def actions(board, pos, dir):
+LOOPS = 0
 
+def search_loop(board, pos, dir):
+
+    global LOOPS
+
+    new_pos, new_dir = actions(board, pos, dir+1, SEARCH_LOOPS=False)
+    while True:
+        
+        if actions(board, new_pos, new_dir, SEARCH_LOOPS=False) is None:
+            return None
+
+        new_pos, new_dir = actions(board, new_pos, new_dir, SEARCH_LOOPS=False)
+        if dir % 2 == 0: #if dir we first bumped == vertical
+            if new_pos[1] == pos[1]:
+                LOOPS += 1
+                return None
+        else: #if dir we first bumped == horizontal
+            if new_pos[0] == pos[0]:
+                LOOPS += 1
+                return None
+        if new_dir == dir+4:
+            return None
+
+
+def actions(board, pos, dir, SEARCH_LOOPS):
+    
     directions = [
         [-1,0], [0, 1],
         [1, 0], [0,-1],
     ]
 
-    new_pos_x = pos[0] + directions[dir%4][0]
-    new_pos_y = pos[1] + directions[dir%4][1]
+    new_pos_x = pos[0] + directions[dir % 4][0]
+    new_pos_y = pos[1] + directions[dir % 4][1]
+
     try:
     #if obstruction, change dir to right
         if board[new_pos_x][new_pos_y] == '#':
-            return actions(board, pos, dir+1)
+            if SEARCH_LOOPS == True:
+                search_loop(board, pos, dir)
+            return actions(board, pos, dir+1, SEARCH_LOOPS=False)
+
     #if out of bounds, finished the maze. stop everything.
     except: return None
 
@@ -18,7 +47,7 @@ def actions(board, pos, dir):
     return ((new_pos_x, new_pos_y), dir)
 
 
-def result(board, pos, action):
+def result(board, pos):
     board[pos[0]][pos[1]] = 'X'
     return board
 
@@ -32,15 +61,15 @@ def init_pos(board):
 
 def main():
 
-    board = [list(line) for line in open("../input.txt").read().split('\n')]
+    board = [list(line) for line in open("../demo.txt").read().split('\n')]
     pos = init_pos(board)
     dir = 0
 
-    while actions(board, pos, dir) is not None:
-        action, dir = actions(board, pos, dir)
-        board = result(board, pos, action)
+    while actions(board, pos, dir, SEARCH_LOOPS=False) is not None:
+        action, dir = actions(board, pos, dir, SEARCH_LOOPS=True)
+        board = result(board, pos)
         pos = action
 
-    print(sum([i.count('X') for i in board])+1)
+    print(LOOPS)
 
 main()
